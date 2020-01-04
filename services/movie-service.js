9fc09ff25390360917ddcf58499c74a8ec62d8f4
Predrag Popovic <pedja.popovic9@gmail.com>
2020-01-04 23:59:38 +0100

    Add method for searching movies

--- services/movie-service.js
+++ services/movie-service.js
@@ -1,16 +1,25 @@
 import fetch from "isomorphic-unfetch";
 
-const BASE_URL = "https://api.themoviedb.org/3/movie";
+const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = "86587b6bca657caa380766ce8dc8906b";
 
 const getTopRatedMovies = async ({ page = 1 }) => {
   const res = await fetch(
-    `${BASE_URL}/top_rated?api_key=${API_KEY}&language=en-US&page=${page}`
+    `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US&page=${page}`
+  );
+  const data = await res.json();
+  return data;
+};
+
+const searchMovies = async ({ query = "", page = 1 }) => {
+  const res = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=${page}&include_adult=false`
   );
   const data = await res.json();
   return data;
 };
 
 export default {
-  getTopRatedMovies
+  getTopRatedMovies,
+  searchMovies
 };

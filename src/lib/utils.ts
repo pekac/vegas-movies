e16fb7a06e9d709a037b4288c41e8b669c91ca74
Predrag Popovic <pedja.popovic9@gmail.com>
2023-03-09 22:02:18 +0100

    Add types to utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,19 @@
+export function findItemIndexInList(list: any[], item: any) {
+  return list.findIndex((i) => i.id === item.id);
+}
+
+export function debounce(fn: Function, interval: number) {
+  let timeout = null;
+
+  return function (...args: any[]) {
+    const context = this;
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => {
+      timeout = null;
+      fn.apply(context, args);
+    }, interval);
+  };
+}

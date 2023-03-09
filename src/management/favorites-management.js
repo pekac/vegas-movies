75a76cee7f62a1db8bc5000d2bf45a92e38b1f63
Predrag Popovic <pedja.popovic9@gmail.com>
2023-03-09 22:54:29 +0100

    Delete entity managements

--- src/management/favorites-management.js
+++
@@ -1,7 +0,0 @@
-import Resource from "./resource";
-
-export default class FavoritesManagement extends Resource {
-  constructor(list) {
-    super(list);
-  }
-}
--- src/management/watch-later-management.js
+++
@@ -1,7 +0,0 @@
-import Resource from "./resource";
-
-export default class WatchLaterManagement extends Resource {
-  constructor(list) {
-    super(list);
-  }
-}

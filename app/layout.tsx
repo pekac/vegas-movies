47bf68021b3f5cf9a5dbcd2a75ff7ee40c2bf057
Predrag Popovic <pedja.popovic9@gmail.com>
2023-03-15 22:42:55 +0100

    Move data fetching to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import "./globals.css";
 import styles from "./styles.module.css";
 
-import Header from "@/app/components/header";
-import FooterNav from "@/app/components/footer-nav";
+import Header from "@components/header";
+import FooterNav from "@components/footer-nav";
+
+import MoviesProvider from "@context/movie";
+
+import { gql, db } from "@lib/graphql";
 
 export const metadata = {
   title: "Vegas Movies - Entertainment's finest",
@@ -11,16 +15,38 @@ export const metadata = {
   },
 };
 
-function Layout({ children }) {
+async function Layout({ children }) {
+  // @ts-ignore
+  const { movies } = await db.request(GetMovies);
   return (
     <html lang="en">
       <body>
-        <Header />
-        <main className={styles.container}>{children}</main>
-        <FooterNav />
+        <MoviesProvider allMovies={movies}>
+          <Header />
+          <main className={styles.container}>{children}</main>
+          <FooterNav />
+        </MoviesProvider>
       </body>
     </html>
   );
 }
+
+const GetMovies = gql`
+  query GetMovies {
+    movies {
+      backdrop: backdrop_path
+      id
+      lang: original_language
+      overview
+      imgSrc: poster_path
+      releaseDate: release_date
+      title
+      rating: vote_average
+      voteCount: vote_count
+      isFavorite: is_favorite
+      onWatchlist: on_watchlist
+    }
+  }
+`;
 
 export default Layout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,7 @@
-import MovieCard from "@/app/components/movie-card";
-import NoResults from "@/app/components/no-result";
-
-import { gql, db } from "@/app/lib/graphql";
+import MovieList from "@components/movie-list";
 
 async function IndexPage() {
-  // @ts-ignore
-  const { allMovies } = await db.request(GetAllMovies, {
-    first: 20,
-  });
-  const { data: movies } = allMovies;
-
-  if (movies.length === 0) {
-    return <NoResults />;
-  }
-
-  return movies.map((movie) => <MovieCard key={movie.id} movie={movie} />);
+  return <MovieList />;
 }
-
-const GetAllMovies = gql`
-  query GetAllMovies($first: Int!) {
-    allMovies(_size: $first) {
-      data {
-        id
-        is_favorite
-        original_title
-        overview
-        poster_path
-        release_date
-        vote_average
-        watchlist
-      }
-    }
-  }
-`;
 
 export default IndexPage;

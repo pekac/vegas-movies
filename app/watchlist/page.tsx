2af12eeb7f0be6935c08084111602b897b114f1a
Predrag Popovic <pedja.popovic9@gmail.com>
2023-03-15 22:52:09 +0100

    Watchlist page wip

--- app/watchlist/page.tsx
+++ app/watchlist/page.tsx
@@ -1,14 +1,41 @@
 import MovieCard from "@/app/components/movie-card";
 import NoResults from "@/app/components/no-result";
 
+import { gql, db } from "@lib/graphql";
+
 async function WatchlistPage() {
-  // const movies = [];
+  // @ts-ignore
+  const { movies } = await db.request(GetMovies);
 
-  // if (!movies || movies?.length === 0) {
-  return <NoResults />;
-  // }
+  if (!movies || movies?.length === 0) {
+    return <NoResults />;
+  }
 
-  // return movies.map((movie) => <MovieCard key={movie.id} movie={movie} />);
+  return (
+    <>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </>
+  );
 }
+
+const GetMovies = gql`
+  query GetMovies {
+    movies {
+      backdrop_path
+      id
+      original_language
+      overview
+      poster_path
+      release_date
+      title
+      vote_average
+      vote_count
+      on_watchlist
+      is_favorite
+    }
+  }
+`;
 
 export default WatchlistPage;

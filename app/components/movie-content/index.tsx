5f39af48cb88c5017e491bde7f7e58882cb179db
Predrag Popovic <pedja.popovic9@gmail.com>
2023-03-17 23:10:00 +0100

    Remove redundant styles

--- app/components/movie-content/index.tsx
+++ app/components/movie-content/index.tsx
@@ -11,12 +11,12 @@ function MovieContent({ overview, rating, releaseDate, title }: Props) {
       <h1 className="text-xl font-normal text-grey-100 text-ellipsis overflow-hidden">
         {title}
       </h1>
-      <p className="my-1 mr-2 text-xs font-bold text-grey-400 float-left">
+      <p className="my-1 text-xs font-bold text-grey-400 float-left">
         Release date: {releaseDate}
       </p>
       <div className="w-full flex justify-between items-center">
         <h5 className="text-xs text-grey-100">SUMMARY</h5>
-        <div className="mr-2 text-base font-bold text-red-400 float-right">
+        <div className="text-base font-bold text-red-400 float-right">
           {rating}/10
         </div>
       </div>

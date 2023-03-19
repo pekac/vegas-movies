85fded1ce0c0da745a7f4cc18b33f7a11174225b
Predrag Popovic <pedja.popovic9@gmail.com>
2023-03-19 01:57:00 +0100

    Add play icon

--- app/components/trailer-btn/index.tsx
+++ app/components/trailer-btn/index.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-// i {
-//   position: relative;
-//   float: left;
-//   line-height: 1;
-// }
-
 function TrailerBtn() {
   const showTrailer = () => {};
   return (
     <button
-      className="px-2 py-1 border border-solid border-red-400 rounded-xs"
+      className="px-2 py-1 flex justify-between items-center border border-solid border-red-400 rounded-xs"
       onClick={showTrailer}
     >
-      <h3 className="text-sm font-normal text-red-400">
-        {/* play icon */}
-        WATCH TRAILER
-      </h3>
+      <img
+        className="pr-1 !h-[20px] !w-[20px]"
+        title="Play trailer"
+        alt="Play trailer btn"
+        src="/icons/play.svg"
+      />
+      <h3 className="text-sm font-normal text-red-400">WATCH TRAILER</h3>
     </button>
   );
 }

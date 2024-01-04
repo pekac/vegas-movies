123b76b07c090e7a19e593a0806af9e51fe4b3a8
Predrag Popovic <pedja.popovic9@gmail.com>
2024-01-04 15:09:44 +0100

    Move data fetching directly to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import MovieList from "@components/movie-list";
 
+import { db } from "@lib/graphql";
+import { IMovie } from "@models/movie";
+
+import { GetMovies } from "@queries/movies";
+
 async function IndexPage() {
-  return <MovieList type="all" />;
+  const { movies } = await db.request(GetMovies) as {movies: IMovie[]};
+  return <MovieList movies={movies} />;
 }
 
 export default IndexPage;

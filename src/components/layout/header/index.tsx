0afd17574bee9ab1929f342db58c0d42f033e9a6
Predrag Popovic <pedja.popovic9@gmail.com>
2024-03-02 23:34:40 +0100

    Increase p-x in header

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -5,7 +5,7 @@ import { SearchInput } from "@components/layout";
 
 export function Header() {
   return (
-    <header className="fixed inset-x-0 top-0 py-1 px-2 h-[60px] flex justify-between items-center bg-gray-800 border-b border-gray-600 z-10">
+    <header className="fixed inset-x-0 top-0 py-1 px-4 h-[60px] flex justify-between items-center bg-gray-800 border-b border-gray-600 z-10">
       <Logo />
       <SearchInput />
       <Link href="/watchlist">
